80d526331f9298cbabf6e84e04a15826ceae6964
amrrs <1littlecoder@gmail.com>
2021-09-09 00:27:04 +0530

    updated caption

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ if image is not None:
 else:
     st.write("Upload an Image")
 
-st.caption("Made with ❤️ by @1littlecoder")
+st.caption("Made with ❤️ by @1littlecoder. Credits to 🤗 Spaces for Hosting this ")
